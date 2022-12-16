fe9edb04ac046806ebaba1188b245dbb9abe8db4
slivcaigor <slivcaigor@gmail.com>
2022-12-16 15:20:15 +0100

    search works for movies and tv series

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -8,12 +8,13 @@ export default {
 
 <template>
   <div class="card h-100">
-    <img :src="`https://image.tmdb.org/t/p/w500${info.poster_path}`" class="card-img-top" :alt="info.title">
+    <img :src="`https://image.tmdb.org/t/p/original${info.poster_path}`" class="card-img-top" :alt="info.title">
     <div class="card-body">
       <h5 class="card-title">{{ info.title }}</h5>
       <p class="card-text">{{ info.original_title }}</p>
       <p class="card-text">{{ info.vote_average }}</p>
       <p class="card-text">{{ info.overview }}</p>
+      <p class="card-text">{{ info.original_language }}</p>
     </div>
   </div>
 </template>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { reactive } from "vue";
 
 export const store = reactive({
   MovieList: [],
-  apiURL: "https://api.themoviedb.org/3/search/movie?api_key=a8c539ffcddf0e1165c359cbff81d1f2&query=batman",
+  type: ["movie", "tv"],
+  apiURL: "https://api.themoviedb.org/3/search/multi?api_key=a8c539ffcddf0e1165c359cbff81d1f2&query=r",
   searchFilm: "",
 });
 
